5c88043b289eebf070da385a042aeb6e54c311e4
treason <treason@foo.bar>
2013-03-21 17:26:34 -0400

    new deb ugh

--- buildDEB.sh
+++ buildDEB.sh
@@ -4,9 +4,9 @@ cd /Users/Jed/Documents/gesturePad
 
 find . -name .DS_Store -ls -exec rm {} \;
 
-rm -r /Users/Jed/Documents/gesturePad/cydia/gesturePad/Applications/gesturePad.app
+rm -r /Users/Jed/Documents/gesturePad/cydia/var/mobile/Applications/gesturePad/gesturePad.app
 
-cp -r /Users/Jed/Documents/gesturePad/build/gesturePad/Build/Products/Debug-iphoneos/gesturePad.app /Users/Jed/Documents/gesturePad/cydia/gesturePad/Applications/gesturePad.app
+cp -r /Users/Jed/Documents/gesturePad/build/gesturePad/Build/Products/Debug-iphoneos/gesturePad.app /Users/Jed/Documents/gesturePad/cydia/gesturePad/var/mobile/Applications/gesturePad/gesturePad.app
 
 cd /Users/Jed/Documents/gesturePad/cydia/
 
